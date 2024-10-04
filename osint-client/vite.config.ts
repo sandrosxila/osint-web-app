1cbe01135217186afe15264f46465786eb0ed30a
sandrosxila <sandrosxila@yahoo.com>
2024-10-04 13:43:50 +0400

    fixing proxy config for vite

--- osint-client/vite.config.ts
+++ osint-client/vite.config.ts
@@ -1,31 +1,37 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import svgr from "vite-plugin-svgr";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react(), svgr()],
-  server: {
-    port: 3000,
-    strictPort: true,
-    host: true,
-    origin: "http://0.0.0.0:3000",
-    proxy: {
-      "/osint": {
-        target: "http://backend:8080",
-        changeOrigin: true,
-        secure: false,
+export default defineConfig(({mode}) => {
+  const env = loadEnv(mode, process.cwd());
+
+  console.log(mode, env);
+
+  return ({
+    plugins: [react(), svgr()],
+    server: {
+      port: 3000,
+      strictPort: true,
+      host: true,
+      origin: "http://0.0.0.0:3000",
+      proxy: {
+        "/osint": {
+          target: env.VITE_TARGET_PROXY,
+          changeOrigin: true,
+          secure: false,
+        },
       },
     },
-  },
-  resolve: {
-    alias: [{ find: "@", replacement: "/src" }],
-  },
-  css: {
-    preprocessorOptions: {
-      scss: {
-        api: "modern-compiler",
+    resolve: {
+      alias: [{ find: "@", replacement: "/src" }],
+    },
+    css: {
+      preprocessorOptions: {
+        scss: {
+          api: "modern-compiler",
+        },
       },
     },
-  },
+  })
 });
